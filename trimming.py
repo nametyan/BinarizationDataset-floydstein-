7ec7e5822e8f8631c12df923b5e5b12c9c54cb1f
nametyan <57559959+nametyan@users.noreply.github.com>
2019-11-10 12:27:17 +0900

    Update trimming.py

--- trimming.py
+++ trimming.py
@@ -14,10 +14,10 @@ import glob
 path = glob.glob("C:/FSdata/BIGpicture/*")#サイズ調整前の画像の格納フォルダ
 path2 = "C:/FSdata/cutpicture"#サイズ調整後の画像の格納フォルダ
 cut_num = 128#扱いたい画像サイズ[pixel]
+makecount = 0#作成した画像の枚数
 
 if(__name__ == "__main__"):
-    for fname in path:
-        makecount = 0#作成した画像の枚数
+    for fname in path:        
         img = cv2.imread(fname)#大きい画像の読み取り        
         height, width, channels = img.shape#サイズ取得
         h_num = int(height//cut_num)#height切る回数
@@ -25,5 +25,5 @@ if(__name__ == "__main__"):
         for h in range(h_num):#y軸
             for w in range(w_num):#x軸
                 clp = img[cut_num * h : cut_num * (h + 1), cut_num * w : cut_num * (w + 1)]#画像を分割できる数とcut_numの積が分割する点になる
-                cv2.imwrite(os.path.join(path2, os.path.basename(fname) + "_cut_" + str(makecount) + ".jpg"), clp)
+                cv2.imwrite(os.path.join(path2, "A_" + str(makecount) + ".jpg"), clp)
                 makecount += 1
