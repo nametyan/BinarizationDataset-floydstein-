047ae3539599734c318fdb068400122ae768950f
nametyan <57559959+nametyan@users.noreply.github.com>
2019-11-11 09:59:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ datacount = 0#作成した画像の枚数
 def floyd(file):
     img_gray = cv2.imread(file, cv2.IMREAD_GRAYSCALE)#グレースケールで読み込み
     img_size = (s, s, 1)#読み込む画像サイズ
-    img_floyd = np.zeros(img_size, dtype = np.uint8)#ディザリング結果
-    img_err = np.zeros(img_size, dtype = np.float16)#誤差拡散用配列
+    img_floyd = np.zeros(img_size, dtype = np.int16)#ディザリング結果
     pix_err = 0#次のピクセルに渡す誤差
     X1 = 7 / 16
     Y1X_1 = 3 / 16
@@ -31,21 +30,21 @@ def floyd(file):
     Y1X1 = 1/ 16#各ピクセルの係数(今回はフロイドスタインバーグ)
     
     for y in range(s - 1):#y軸
-        for x in range(s - 1):#x軸            
-            if((img_gray[y][x] + int(img_err[y][x])) < threshold_pix):#閾値より小さい
+        for x in range(s - 1):#x軸        
+            if((img_gray[y][x] + img_floyd[y][x]) < threshold_pix):#閾値より小さい
                 img_floyd[y][x] = min_pix#白を表示
                 pix_err = img_gray[y][x]#誤差に代入         
-                img_err[y][x + 1] += int(X1 * pix_err)                   
-                img_err[y + 1][x + 1] += int(Y1X1 * pix_err) 
-                img_err[y + 1][x] += int(Y1 * pix_err)
-                img_err[y + 1][x - 1] += int(Y1X_1 * pix_err)                      
+                img_floyd[y][x + 1] += int(X1 * pix_err)                   
+                img_floyd[y + 1][x + 1] += int(Y1X1 * pix_err) 
+                img_floyd[y + 1][x] += int(Y1 * pix_err)
+                img_floyd[y + 1][x - 1] += int(Y1X_1 * pix_err)                      
             else:#閾値より大きい
                 img_floyd[y][x] = max_pix#黒を表示
                 pix_err = img_gray[y][x] - max_pix#差を誤差に代入               
-                img_err[y][x + 1] += int(X1 * pix_err)                    
-                img_err[y + 1][x + 1] += int(Y1X1 * pix_err)                
-                img_err[y + 1][x] += int(Y1 * pix_err)               
-                img_err[y + 1][x - 1] += int(Y1X_1 * pix_err)
+                img_floyd[y][x + 1] += int(X1 * pix_err)                    
+                img_floyd[y + 1][x + 1] += int(Y1X1 * pix_err)                
+                img_floyd[y + 1][x] += int(Y1 * pix_err)               
+                img_floyd[y + 1][x - 1] += int(Y1X_1 * pix_err)        
     return img_floyd
    
 if(__name__ == '__main__'):
